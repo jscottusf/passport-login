2f30c021cd51dd01837ce110141cf5f2868b2c14
Joel Scott <jscottusf@gmail.com>
2020-03-28 21:33:47 -0400

    authentication completed

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
+const authUtils = require("./utils/auth");
+const MongoClient = require("mongodb").MongoClient;
+const Strategy = require("passport-local").Strategy;
+const ObjectID = require("mongodb").ObjectID;
 
 app.use(flash());
 app.use(
@@ -29,17 +33,61 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
 
-const users = [];
+//"mongodb+srv://jscottusf:IgPj2uoehQiVellZ@cluster0-giujn.mongodb.net/test?retryWrites=true&w=majority"
+//'mongodb:/ / localhost'
+MongoClient.connect(
+  process.env.DATABASE_URL,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      throw err;
+    }
 
-const initializePassport = require("./passport-config");
-initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+    const db = client.db("crishipped-users");
+    const users = db.collection("users");
+    app.locals.users = users;
+  }
 );
 
+passport.use(
+  new Strategy((email, password, done) => {
+    app.locals.users.findOne({ email }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      if (user.password != authUtils.hashPassword(password)) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, { id });
+});
+
 app.get("/", checkAuthenticated, (req, res) => {
-  res.render("index", { name: req.user.username });
+  const users = req.app.locals.users;
+  const _id = ObjectID(req.session.passport.user);
+  console.log(_id);
+
+  users.findOne({ _id }, (err, results) => {
+    if (err || !results) {
+      res.render("index", { messages: { error: ["User not found"] } });
+    }
+    res.render("index", { name: results.name });
+  });
 });
 
 app.get("/login", checkNotAuthenticated, (req, res) => {
@@ -50,30 +98,36 @@ app.post(
   "/login",
   checkNotAuthenticated,
   passport.authenticate("local", {
-    successRedirect: "/",
     failureRedirect: "/login",
-    failureFlash: true
-  })
+    failureFlash: "wrong username or password"
+  }),
+  (req, res, next) => {
+    res.redirect("/");
+  }
 );
 
 app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register");
 });
 
-app.post("/register", checkNotAuthenticated, async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword
-    });
+app.post("/register", checkNotAuthenticated, (req, res, next) => {
+  //const hashedPassword = bcrypt.hash(req.body.password, 10);
+  const registrationParams = req.body;
+  const users = req.app.locals.users;
+  const payload = {
+    name: registrationParams.name,
+    email: registrationParams.email,
+    password: authUtils.hashPassword(registrationParams.password)
+  };
+
+  users.insertOne(payload, err => {
+    if (err) {
+      req.flash("error", "User account already exists");
+    } else {
+      req.flash("success", "User account registered successfully");
+    }
     res.redirect("/login");
-  } catch {
-    res.redirect("/register");
-  }
-  console.log(users);
+  });
 });
 
 function checkAuthenticated(req, res, next) {
